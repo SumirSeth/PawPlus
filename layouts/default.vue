6d8f1d76e7786d1f48938c9b0de224a4d07dd94d
Sumir Seth <iamsumirseth@gmail.com>
2024-10-12 20:27:59 +0530

    mobile changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,8 @@
     </div> -->
     <nav class="fixed top-0 left-0 right-0 z-50 h-20 flex items-center justify-center">
       <div class="bg-black/5 dark:bg-white/5 backdrop-brightness-110 backdrop-blur-xl shadow-lg flex items-center space-x-6 px-6 py-2 rounded-2xl max-w-max">
-        <div class="text-2xl font-general font-light dark:text-white"><NuxtLink to="/">PawPlus</NuxtLink></div>
-        <ul class="flex space-x-6 font-general-italic font-normal dark:text-white items-center ">
+        <div class="text-lg font-general font-light dark:text-white"><NuxtLink to="/">PawPlus</NuxtLink></div>
+        <ul class="flex space-x-6 font-general-italic font-normal dark:text-white items-center text-sm ">
           <li><NuxtLink to="/services" class="">services</NuxtLink></li>
           <li><NuxtLink to="/about" class="">about</NuxtLink></li>
         </ul>
